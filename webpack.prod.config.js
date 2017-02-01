88ef2ee2fedc643be71a83db7c3ee3d5881f0224
Filip Danić <filip@spicefactory.co>
2017-02-01 23:24:58 +0100

    update webpack build + dependents

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,21 +1,17 @@
-
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: [
-    path.join(__dirname, 'src/spicy-datatable/SpicyDatatable.js'),
-    path.join(__dirname, 'src/spicy-datatable/table.css')
-  ],
+  entry: path.join(__dirname, 'src/spicy-datatable/SpicyDatatable.js'),
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   output: {
     path: path.join(__dirname, 'lib'),
-    filename: 'index.js',
+    filename: 'SpicyDatatable.js',
     libraryTarget: "umd",
-    library: "SpicyDatatable"
+    library: "SpicyDatatable",
+    umdNamedDefine: true
   },
   externals: {
     "react": {
@@ -34,7 +30,6 @@ module.exports = {
     }
   },
   plugins: [
-    new ExtractTextPlugin('spicy-datatable.css'),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
@@ -53,10 +48,6 @@ module.exports = {
         loaders: ['babel'],
         include: [path.join(__dirname, 'src')],
         exclude: path.resolve(__dirname, 'node_modules'),
-      },
-      {
-        test: /\.(css|scss)$/,
-        loader: ExtractTextPlugin.extract('style', 'css')
       }
     ]
   }
